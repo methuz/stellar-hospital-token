781df7decb1fa1dbe53988d4241d71a08ecfc19b
Methuz Kaewsaikao <methuz.k@gmail.com>
2018-03-04 00:17:44 +0700

    Refactor escrow account

--- 2_escrow_account.js
+++ 2_escrow_account.js
@@ -1,88 +1,86 @@
-const StellarSdk = require('stellar-sdk');
-const YAML = require('node-yaml');
+const StellarSdk = require('stellar-sdk')
+const YAML = require('node-yaml')
 
-const configFile = 'config.yml';
+const configFile = 'config.yml'
 
-const config = YAML.readSync(configFile);
+const config = YAML.readSync(configFile)
 
-const newKey = StellarSdk.Keypair.random();
-const startingFund = "100";
+const newKey = StellarSdk.Keypair.random()
 
-console.log("public : ", newKey.publicKey());
+console.log('public : ', newKey.publicKey())
 
-var server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
-var govKey = StellarSdk.Keypair.fromSecret(config.accounts.gov.secret);
-var ngoKey = StellarSdk.Keypair.fromSecret(config.accounts.ngo.secret);
+var server = new StellarSdk.Server('https://horizon-testnet.stellar.org')
+var govKey = StellarSdk.Keypair.fromSecret(config.accounts.gov.secret)
+var ngoKey = StellarSdk.Keypair.fromSecret(config.accounts.ngo.secret)
 
-StellarSdk.Network.useTestNetwork();
+StellarSdk.Network.useTestNetwork()
 
-function govCreateAccount() {
-  console.log("creating account");
-  return server.loadAccount(govKey.publicKey()).then(function(govAccount) {
-    const transaction = new StellarSdk.TransactionBuilder(govAccount)
-      .addOperation(StellarSdk.Operation.createAccount({
-        destination: newKey.publicKey(),
-        startingBalance: startingFund
-      }))
-      .build();
-    transaction.sign(govKey);
-    return server.submitTransaction(transaction);
-  })
+async function govCreateAccount (startingBalance) {
+  startingBalance = typeof startingBalance === 'string' ? startingBalance : startingBalance.toString()
+  const govAccount = await server.loadAccount(govKey.publicKey())
+  const transaction = new StellarSdk.TransactionBuilder(govAccount)
+    .addOperation(StellarSdk.Operation.createAccount({
+      destination: newKey.publicKey(),
+      startingBalance
+    }))
+    .build()
+  transaction.sign(govKey)
+  return server.submitTransaction(transaction)
 }
 
-function setJointAccount() {
-  console.log("Set Join Account");
-  return server.loadAccount(newKey.publicKey()).then(function(escrowAccount) {
-    const transaction = new StellarSdk.TransactionBuilder(escrowAccount)
-      .addOperation(StellarSdk.Operation.setOptions({
-        signer: {
-          ed25519PublicKey: govKey.publicKey(),
-          weight: 1
-        },
-      }))
-      .addOperation(StellarSdk.Operation.setOptions({
-        masterWeight: 0,
-        lowThreshold: 2,
-        medThreshold: 2,
-        highThreshold: 2,
-        signer: {
-          ed25519PublicKey: ngoKey.publicKey(),
-          weight: 1
-        },
-      }))
-      .build();
-    transaction.sign(newKey);
-    return server.submitTransaction(transaction);
-  })
+async function setJointAccount () {
+  console.log('setting joint account')
+  const escrowAccount = await server.loadAccount(newKey.publicKey())
+  const transaction = new StellarSdk.TransactionBuilder(escrowAccount)
+    .addOperation(StellarSdk.Operation.setOptions({
+      signer: {
+        ed25519PublicKey: govKey.publicKey(),
+        weight: 1
+      }
+    }))
+    .addOperation(StellarSdk.Operation.setOptions({
+      masterWeight: 0,
+      lowThreshold: 2,
+      medThreshold: 2,
+      highThreshold: 2,
+      signer: {
+        ed25519PublicKey: ngoKey.publicKey(),
+        weight: 1
+      }
+    }))
+    .build()
+  transaction.sign(newKey)
+  return server.submitTransaction(transaction)
 }
 
-function ngoSendFund() {
-  console.log("NGO send fund");
-  return server.loadAccount(ngoKey.publicKey()).then(function(ngoAccount) {
-    const transaction = new StellarSdk.TransactionBuilder(ngoAccount)
-      .addOperation(StellarSdk.Operation.payment({
-        destination: newKey.publicKey(),
-        asset: StellarSdk.Asset.native(),
-        amount: startingFund
-      }))
-      .build();
-    transaction.sign(ngoKey);
-    return server.submitTransaction(transaction);
-  })
+async function sendFund (from, to, amount) {
+  amount = typeof amount === 'string' ? amount : amount.toString()
+  const fromAccount = await server.loadAccount(from.publicKey())
+  const transaction = new StellarSdk.TransactionBuilder(fromAccount)
+    .addOperation(StellarSdk.Operation.payment({
+      destination: to.publicKey(),
+      asset: StellarSdk.Asset.native(),
+      amount
+    }))
+    .build()
+  transaction.sign(from)
+  return server.submitTransaction(transaction)
 }
 
-async function getEscrowAccount() {
+async function getEscrowAccount () {
   try {
-    saveConfig()
-    await govCreateAccount()
+    saveConfigSync()
+    await govCreateAccount('5')
     await setJointAccount()
-    await ngoSendFund()
+    await sendFund(ngoKey, newKey, 100)
+    await sendFund(govKey, newKey, 95)
+    console.log('Setting escrow account success!!');
   } catch (error) {
-    console.log("error = ", JSON.stringify(error, null, 4));
+      console.log("error = ", JSON.stringify(error, null, 4));
   }
 }
 
-function saveConfig() {
+function saveConfigSync () {
   config['accounts']['joint_account'] = {
     public: newKey.publicKey(),
     secret: newKey.secret()
